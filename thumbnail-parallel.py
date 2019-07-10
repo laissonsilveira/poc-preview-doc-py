d433c114fad03440a487cb696e026439f6fa336d
Laisson Rangel Silveira <laisson.silveira@digitro.com.br>
2019-07-10 17:32:01 -0300

    Update

--- thumbnail-parallel.py
+++ thumbnail-parallel.py
@@ -1,36 +1,24 @@
 from preview_generator.manager import PreviewManager
 from os import listdir, stat
 from multiprocessing import Pool
+from shutil import rmtree
 import time
 
 start_total = time.time()
 
-manager = PreviewManager("preview", create_folder=True)
+pathPreview = "preview"
+rmtree(pathPreview)
+
+manager = PreviewManager(pathPreview, create_folder=True)
 
 def make_preview(file):
         path_file = "files-to-convert/" + file
-        statinfo = stat(path_file)
-        print("File '%s' - size: %s bytes" % (file, statinfo.st_size))
-
         start = time.time()
-        path_to_preview_image = manager.get_jpeg_preview(path_file)
+        path_to_preview_image = manager.get_jpeg_preview(path_file, width=140)
         end = time.time()
-        print("%s: %s seconds" % (path_to_preview_image, end - start))
-
-        start = time.time()
-        path_to_preview_image = manager.get_jpeg_preview(
-        path_file, width=140)
-        end = time.time()
-        print("%s: %s seconds" % (path_to_preview_image, end - start))
-
-        start = time.time()
-        path_to_preview_image = manager.get_jpeg_preview(
-        path_file, width=140, page=0)
-        end = time.time()
-        print("One page - %s: %s seconds" % (path_to_preview_image, end - start))
+        print("PREVIEW -> %s: %s seconds" % (path_to_preview_image.replace(pathPreview + "/", ""), end - start))
         print("--------------------")
 
-            
 p = Pool(4)
 p.map(make_preview, listdir("files-to-convert"))
 
--- thumbnail.py
+++ thumbnail.py
@@ -1,34 +1,36 @@
 import time, os
+from shutil import rmtree
+
 start_total = time.time()
 
 from preview_generator.manager import PreviewManager
 
-manager = PreviewManager("preview", create_folder= True)
+pathPreview = "preview"
+rmtree(pathPreview)
+
+manager = PreviewManager(pathPreview, create_folder= True)
 
 total_size = 0
+total_size_preview = 0
 for r, d, f in os.walk("files-to-convert"):
     for file in f:
         path_file = "files-to-convert/" + file
-        statinfo = os.stat(path_file)
-        print("File '%s' - size: %s bytes" % (file, statinfo.st_size))
-        total_size = total_size + statinfo.st_size
-
-        start = time.time()
-        path_to_preview_image = manager.get_jpeg_preview(path_file)
-        end = time.time()
-        print("%s: %s seconds" % (path_to_preview_image, end - start))
+        size = os.stat(path_file).st_size
+        print("FILE -> '%s' - size: %s bytes" % (file, size))
+        total_size = total_size + size
 
         start = time.time()
         path_to_preview_image = manager.get_jpeg_preview(path_file, width=140)
         end = time.time()
-        print("%s: %s seconds" % (path_to_preview_image, end - start))
 
-        start = time.time()
-        path_to_preview_image = manager.get_jpeg_preview(path_file, width=140, page=0)
-        end = time.time()
-        print("One page - %s: %s seconds" % (path_to_preview_image, end - start))
+        size_preview = os.stat(path_to_preview_image).st_size
+        total_size_preview = total_size_preview + size_preview
+        
+        print("PREVIEW -> %s: %s seconds / size: %s bytes" % (path_to_preview_image.replace(pathPreview + "/", ""), end - start, size_preview))
         print("--------------------")
 
 end_total = time.time()
+
 print("Total size: %s bytes" % (total_size))
+print("Total size preview: %s bytes" % (total_size_preview))
 print("Total time: %s seconds" % (end_total - start_total))

903728828fec1ca7bf912cf8de6529531c21ba13
Laisson R. Silveira <laisson.r.silveira@gmail.com>
2019-09-26 16:59:32 -0300

    Update preview-doc.py

--- preview-doc.py
+++ preview-doc.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 import sys
+from os import path, rename
+from shutil import rmtree
 from preview_generator.manager import PreviewManager
 
-file_in = sys.argv[1]
-file_out = sys.argv[2]
+cache_path = "./tmp"
 
-manager = PreviewManager(file_out, create_folder= True)
-path_to_preview_image = manager.get_jpeg_preview(file_in, width=140)
+if len(sys.argv) < 2 or not(path.isfile(sys.argv[1])):
+    print("Nome ou caminho do arquivo não informado corretamente")
+    sys.exit(1)
 
-print(path_to_preview_image)
+file_source = sys.argv[1]
+file_name = path.basename(file_source)
+
+manager = PreviewManager(cache_path, create_folder= True)
+path_preview = manager.get_jpeg_preview(file_name, width=140)
+
+new_file_name = path.splitext(file_name)[0] + "_thumbnail.jpeg"
+rename(path_preview, new_file_name)
+
+rmtree(cache_path)

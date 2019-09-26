4dd7d36620079c57f352ee336940416bd522e79c
Laisson Rangel Silveira <laisson.silveira@digitro.com>
2019-09-26 14:07:42 -0300

    Update

--- thumbnail.py
+++ thumbnail.py
@@ -6,21 +6,30 @@ start_total = time.time()
 from preview_generator.manager import PreviewManager
 
 pathPreview = "preview"
+pathIn = "files"
 rmtree(pathPreview)
 
 manager = PreviewManager(pathPreview, create_folder= True)
 
 total_size = 0
 total_size_preview = 0
-for r, d, f in os.walk("files-to-convert"):
+for r, d, f in os.walk(pathIn):
     for file in f:
-        path_file = "files-to-convert/" + file
+        path_file = pathIn + "/" + file
         size = os.stat(path_file).st_size
         print("FILE -> '%s' - size: %s bytes" % (file, size))
         total_size = total_size + size
 
         start = time.time()
-        path_to_preview_image = manager.get_jpeg_preview(path_file, width=140)
+        try:
+            path_to_preview_image = manager.get_jpeg_preview(path_file, width=140)
+        except FileNotFoundError as e:
+            print("ERROR -> File[{0}] - FileNotFoundError({1}): {2}".format(file, e.errno, e.strerror))
+        except KeyError as e:
+            print("ERROR -> File[{0}] - KeyError({1}): {2}".format(file, e.errno, e.strerror))
+        except:
+            print("ERROR -> File[{0}] - Error Desconhecido".format(file))
+        
         end = time.time()
 
         size_preview = os.stat(path_to_preview_image).st_size

94ccb981ee059bc7559bb3248428c6d2716bed41
Laisson R. Silveira <laisson.r.silveira@gmail.com>
2019-09-27 20:08:02 -0300

    Update preview-doc.py

--- preview-doc.py
+++ preview-doc.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 import sys
-from os import path, rename
+from os import path, rename, environ
 from shutil import rmtree
 from preview_generator.manager import PreviewManager
 
 cache_path = "./tmp"
 
 if len(sys.argv) < 2 or not(path.isfile(sys.argv[1])):
-    print("Nome ou caminho do arquivo não informado corretamente")
-    sys.exit(1)
+    sys.exit("Nome ou caminho do arquivo não informado corretamente")
 
-file_source = sys.argv[1]
-file_name = path.basename(file_source)
+try:
+    file_source = sys.argv[1]
+    file_name = path.basename(file_source)
 
-manager = PreviewManager(cache_path, create_folder= True)
-path_preview = manager.get_jpeg_preview(file_name, width=140)
+    manager = PreviewManager(cache_path, create_folder= True)
+    path_preview = manager.get_jpeg_preview(file_name, width=140)
 
-new_file_name = path.splitext(file_name)[0] + "_thumbnail.jpeg"
-rename(path_preview, new_file_name)
+    new_file_name = path.splitext(file_name)[0] + "_thumbnail.jpeg"
+    rename(path_preview, new_file_name)
 
-rmtree(cache_path)
+    rmtree(cache_path)
+except FileNotFoundError as e:
+    error = "File[{0}] - FileNotFoundError({1}): {2}".format(file_source, e.errno, e.strerror)
+    sys.exit(error)
+except:
+    error = "File[{0}] - Error Desconhecido".format(file_source)
+    sys.exit(error)
